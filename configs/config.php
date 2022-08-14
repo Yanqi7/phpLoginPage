e789bb7969ce1cdfc9ffb231009481240972405b
Yanqi <17521536852@163.com>
2022-08-14 19:04:39 +0800

    minor repair

--- configs/config.php
+++ configs/config.php
@@ -3,7 +3,7 @@ $host="localhost";
 $username="root";
 $password="root";
 $dbname="userlogindb";
-$conn=mysqli_connect($username,$password,$dbname);
+$conn=mysqli_connect($host,$username,$password,$dbname);
 if($conn->connect_error){
     die("数据库连接失败".$conn->connect_error);
 }
