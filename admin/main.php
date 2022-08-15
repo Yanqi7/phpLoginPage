b993c4fc452bd639408ba7bbeadc86e335b44bdf
Yanqi <17521536852@163.com>
2022-08-15 15:37:18 +0800

    修复session信息读取

--- admin/main.php
+++ admin/main.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once 'init.php';
+session_start();
 
 if(isset($_SESSION['username'])){
     echo $_SESSION['username'];
@@ -8,4 +9,5 @@ if(isset($_SESSION['username'])){
     echo '验证失败';
 }
 
-// 不实际部署无法记录session信息
+// 不实际部署无法记录session信息 -- 划掉
+// 需要添加session_start()即可注册到超全局变量$_SESSION
